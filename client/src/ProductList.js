a12b32c6586aa334be9cf96d377fb96668bb0f0c
Haile-08 <haile4cmd@gmail.com>
2022-07-27 20:24:21 +0300

    created the product compnent

---
+++ client/src/ProductList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Product from "./components/ Product";
+import "../src/CSS/App.css";
+
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="container">
+            <Product />
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default ProductList;
