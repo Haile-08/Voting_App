27642b28ed3d664dd109d970e9361a49caf1149f
Haile-08 <haile4cmd@gmail.com>
2022-07-28 20:58:08 +0300

    created the product

--- client/src/ProductList.js
+++ client/src/ProductList.js
@@ -1,17 +1,25 @@
 import React from "react";
 import Product from "./components/ Product";
 import "../src/CSS/App.css";
-
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
 class ProductList extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <div className="container">
+      <div className="App-header">
+        <CssBaseline />
+        <Container
+          maxWidth="sm"
+          sx={{ bgcolor: "#353b48", borderRadius: "10px" }}
+        >
+          <h2>Popular Product</h2>
+          <hr />
+          <Box sx={{ bgcolor: "#2f3640", height: "auto" }}>
             <Product />
-          </div>
-        </header>
+          </Box>
+        </Container>
       </div>
     );
   }
--- client/src/components/ Product.js
+++ client/src/components/ Product.js
@@ -1,28 +1,49 @@
 import React from "react";
 import "../CSS/product.css";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
 import Avatar from "@mui/material/Avatar";
-import img1 from "../asset/products/image-aqua.png";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import img1 from "../../src/asset/avatars/daniel.jpg";
+import img2 from "../asset/products/image-aqua.png";
 
 class Product extends React.Component {
   render() {
     return (
       <>
-        <h2>Popular Product</h2>
-        <hr />
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar>
-              <img src={img1} />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText
-            primary="Photosdsgasdgasgaerg"
-            secondary="Jan 9, 2014"
+        <Card
+          sx={{
+            maxWidth: 560,
+            marigin: 20,
+            bgcolor: "#dcdde1",
+            borderRadius: 2,
+          }}
+        >
+          <CardHeader
+            avatar={<Avatar src={img1} aria-label="recipe"></Avatar>}
+            title="Shrimp and Chorizo Paella"
+            subheader="September 14, 2016"
           />
-        </ListItem>
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            height="140"
+            image={img2}
+          />
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <IconButton>
+            <ArrowDropUpIcon />
+          </IconButton>
+        </Card>
       </>
     );
   }
